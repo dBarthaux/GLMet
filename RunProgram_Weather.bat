83ece3f67ca4b29da0e1b214ed0ef9f222153871
Dan Barthaux <133030848+dBarthaux@users.noreply.github.com>
2026-03-03 14:19:32 -0500

    Update RunProgram to include comments and formatting

--- RunProgram_Weather.bat
+++ RunProgram_Weather.bat
@@ -1,14 +1,25 @@
 @echo on
+rem Close JPEGView if open
 taskkill /IM JPEGView.exe /F
+
+rem Open a python environment
 call C:\Users\meteouser\miniconda3\Scripts\activate.bat
+
+rem Navigate to directory
 cd C:\Users\meteouser\Documents\GLMet-main
+
+rem Launch data collection and plot manager
 python "CanadaMart.py"
 
+rem Launch JPEGView with the figures folder
 set "folder=C:\Users\meteouser\Documents\GLMet-main\Figures"
 for %%F in ("%folder%\*.png") do (
 	start "" "C:\Program Files\JPEGView64\JPEGView.exe" "%%F"
 	goto :done
 )
 :done
+
+rem Use hotkeys to automatically make fullscreen
 timeout /t 2 /nobreak
 start "" "C:\Users\meteouser\Documents\AutoHotkey\JPEGViewerDoer.exe"
+
