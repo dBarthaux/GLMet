9163ed0bf6347e8ee8ccb022c9d55efed4dd021d
Dan Barthaux <133030848+dBarthaux@users.noreply.github.com>
2026-03-03 14:08:46 -0500

    Delete TurnOffMonitor.ps1

--- TurnOffMonitor.ps1
+++
@@ -1,9 +0,0 @@
-Add-Type -TypeDefinition @"
-using System;
-using System.Runtime.InteropServices;
-public class Monitor {
-    [DllImport("user32.dll", CharSet=CharSet.Auto)]
-    public static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
-}
-"@
-[Monitor]::SendMessage([IntPtr]::Zero, 0x0112, [IntPtr]0xF170, [IntPtr]2)
