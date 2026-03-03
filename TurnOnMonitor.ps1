7505d058a772e6c39cabe5cbd92379f91582a16b
Dan Barthaux <133030848+dBarthaux@users.noreply.github.com>
2026-03-03 14:08:54 -0500

    Delete TurnOnMonitor.ps1

--- TurnOnMonitor.ps1
+++
@@ -1,7 +0,0 @@
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
-
-# Move mouse by 1 pixel and back to wake the screen
-$pos = [System.Windows.Forms.Cursor]::Position
-[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point ($pos.X+1), ($pos.Y+1)
-[System.Windows.Forms.Cursor]::Position = $pos
